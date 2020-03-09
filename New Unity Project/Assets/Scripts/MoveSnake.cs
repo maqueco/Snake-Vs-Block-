78cdc3dead3a9f9b3b164e091b885c4e4baf08d1
Tomas <tomas_uribe_2012@hotmail.com>
2020-03-09 13:23:27 -0300

    Commit 1: Movement
    Basic Movement implemented

---
+++ New Unity Project/Assets/Scripts/MoveSnake.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveSnake : MonoBehaviour
+{
+    private float moveSpeed = 1f;
+    private float speed = .1f;
+    public List<GameObject> snakeParts; //To add more objects
+    private Vector2 lastLocation;
+
+    void Start()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        snakeParts[0].transform.Translate(Vector2.up * moveSpeed * Time.deltaTime);
+        lastLocation = snakeParts[0].transform.position;
+        Camera.main.transform.position = new Vector3(Camera.main.transform.position.x , lastLocation.y, Camera.main.transform.position.z);
+        foreach (GameObject part in snakeParts)
+        {
+            if (part != snakeParts[0])
+            {
+                var newPosition = new Vector2(lastLocation.x, lastLocation.y - 1);
+                lastLocation = part.transform.position;
+                part.transform.position = newPosition;
+            }
+        }
+
+            if (Input.GetAxis("Horizontal") > 0)
+            {
+                if (transform.position.x < 3)
+                {
+                    snakeParts[0].transform.Translate(Vector2.right * speed);
+                }
+            }
+
+            if (Input.GetAxis("Horizontal") < 0)
+            {
+                if(transform.position.x > -3)
+                {
+                    snakeParts[0].transform.Translate(Vector2.left * speed);
+                }
+            }
+        //}
+    }
+}
