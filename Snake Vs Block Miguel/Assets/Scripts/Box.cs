4ec3b475817f99eb69503eaa20b097ab75aae5bc
Tomas Uribe <tomas_uribe_2012@hotmail.com>
2020-03-12 14:03:29 -0300

    Add Scriptable Object Box

---
+++ Snake Vs Block Miguel/Assets/Scripts/Box.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Box", menuName = "Box" )]
+public class Box : ScriptableObject
+{ 
+    //public TextMesh value;
+    public Color BoxColor;
+    public int minLifeRange;
+    public int maxLifeRange;
+}
--- Snake Vs Block Miguel/Assets/Scripts/Cube.cs
+++ Snake Vs Block Miguel/Assets/Scripts/Cube.cs
@@ -5,21 +5,34 @@ using UnityEngine;
 public class Cube : MonoBehaviour
 {
    
+    public Box box;
     private int lifePointsBox;
+    private SpriteRenderer colorBox;
     [SerializeField]
-    private int minRandomBlockLife = 1;
-    [SerializeField]
-    private int maxRandomBLockLife = 10;
- 
+    private TextMesh boxText;
+
     // Generates a random number for fun
+
     private int randomizeLifeBox()
     {
-        lifePointsBox = Random.Range(minRandomBlockLife, maxRandomBLockLife);
+        lifePointsBox = Random.Range(box.minLifeRange, box.maxLifeRange);
+        //boxText.text = lifePointsBox.ToString();
         return lifePointsBox;
     }
     //For Encapsulation
     public int LifeSmasher
     {
         get { return randomizeLifeBox(); }
+    }
+
+    private void ChangeColor()
+    {
+        colorBox.color = box.BoxColor;
+    }
+
+    private void SeeNumberText()
+    {
+        boxText = GetComponentInChildren<TextMesh>();
+        boxText.text = lifePointsBox.ToString();
     }
 }
